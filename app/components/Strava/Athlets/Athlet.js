1ac2748e8100c992afe80462272872afa4e813a1
andresol <andre@sollie.info>
2017-10-16 22:10:04 +0200

    more work

--- app/components/Strava/Athlets/Athlet.js
+++ app/components/Strava/Athlets/Athlet.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Athlet = ({ id, name }) => (
-  <li data-id={id}>{name}</li>
+export const Athlet = ({ id, name, action }) => (
+  <li data-id={id}><button onClick={action}>{name}</button></li>
 );
 
 Athlet.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 Athlet.defaultProps = {
   name: '',
   id: '',
+  action: () => {},
 };
 
 export default Athlet;
--- app/components/Strava/Athlets/index.js
+++ app/components/Strava/Athlets/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Athlet from './Athlet';
 
-export const Athlets = ({ athlets }) => (
+export const Athlets = ({ athlets, action }) => (
   <section id="athlets">
     {athlets.map((a) =>
-      <Athlet key={a.id} name={a.name} id={a.id} />
+      <Athlet key={a.id} name={a.name} id={a.id} action={action} />
     )}
   </section>
 );
 
 Athlets.propTypes = {
   athlets: PropTypes.array.isRequired,
+  action: PropTypes.func.isRequired,
 };
 
 Athlets.defaultProps = {
   athlets: [],
+  action: () => {},
 };
 
 export default Athlets;
